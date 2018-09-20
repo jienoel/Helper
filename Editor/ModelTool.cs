6d4ae664c1401e71d993643fd3280425a8cd1529
Chen Jie <chenjie02@bilibili.com>
2018-09-20 13:52:41 +0800

    加入Mesh分析工具

---
+++ Editor/ModelTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEditor;
+
+public class ModelTool : EditorWindow {
+
+	[MenuItem("Tools/Model/ModelTool")]
+	static void Init()
+	{
+		GameObject go = Selection.activeGameObject;
+		if (go == null)
+		{
+			Debug.LogError("没有选中目标GameObject");
+			return;
+		}
+		
+		Debug.Log(go.transform.GetAllGOCount());
+		List<Tuple<string, int, int>> meshInfos = go.transform.GetAllMeshInfo();
+		Debug.Log(meshInfos.IEnumerableToString((Tuple<string, int, int> value) => { 
+			return string.Format("{0} , verts:{1}, trianles:{2}",value.item1, value.item2, value.item3);
+		}));
+		List<ShaderInfo> shaderinfos = go.transform.GetAllShaderInfo();
+		Debug.Log(string.Format("shaders:{0} \r\n {1}", shaderinfos.Count.ToString(), shaderinfos.IEnumerableToString(
+			(ShaderInfo value) => { return value.shaderName + "  Mat Count:"+value.matGO.Count.ToString();})));
+		Debug.Log(shaderinfos.IEnumerableToString());
+	}
+
+	[MenuItem("Tools/Model/ModelAnalyseWin")]
+	static void OnWindowCreate()
+	{
+		
+	}
+}
